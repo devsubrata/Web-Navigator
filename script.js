c6e5495c3e5a528b746b0ce0823a6463762aadd4
devsubrata <devsubrata@gmail.com>
2025-03-10 13:08:13 +0600

    Multiple Searches feature added

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+const search_links = {
+    "Cambridge dictionary": "https://dictionary.cambridge.org/dictionary/english/{search_term}",
+    "Longman dictionary": "https://www.ldoceonline.com/dictionary/{search_term}",
+    "Oxford dictionary":
+        "https://www.oxfordlearnersdictionaries.com/definition/english/{search_term}",
+    "Webster dictionary": "https://www.merriam-webster.com/dictionary/{search_term}",
+    "Collins dictionary": "https://www.collinsdictionary.com/dictionary/english/{search_term}",
+    "vocabulary.com": "https://www.vocabulary.com/dictionary/{search_term}",
+    "dictionary.com": "https://www.dictionary.com/browse/{search_term}",
+    "Youglish.com": "https://youglish.com/pronounce/{search_term}/english",
+    "Google search": "https://www.google.com/search?q={search_term}",
+    "Image search": "https://www.google.com/search?tbm=isch&q={search_term}",
+    "Video search": "https://www.google.com/search?tbm=vid&q={search_term}",
+    "Google Map": "https://www.google.com/maps/place/{search_term}",
+    Wikipedia: "https://en.wikipedia.org/wiki/{search_term}",
+    "Youtube search": "https://www.youtube.com/results?search_query={search_term}",
+    "Facebook search": "https://www.facebook.com/search/top/?q={search_term}",
+    "News search": "https://www.google.com/search?tbm=nws&q={search_term}",
+    "Pdf search": "https://www.google.com/search?q={search_term}&as_filetype=pdf",
+    Downloadly: "https://downloadlynet.ir/?s={search_term}",
+    GetIntoPC: "https://getintopc.com/?s={search_term}",
+    TorrentEngine: "https://1337x.to/sort-search/{search_term}/time/desc/1/",
+    "Book library": "https://www.libgen.is/search.php?req={search_term}",
+    Github: "https://github.com/search?q={search_term}",
+};
+
 const web_links = {
     ChatGPT: "https://chatgpt.com/",
     DeepSeek: "https://chat.deepseek.com/",
@@ -38,7 +64,10 @@ function navigate() {
 
 function search(url) {
     let search_term = document.getElementById("search_term").value;
-    if (!search_term) return;
+    if (!search_term) {
+        alert("Input box empty!\nEnter your search...");
+        return;
+    }
     window.open(url + search_term, "_blank");
 }
 
@@ -55,6 +84,56 @@ document.getElementById("vocab_btn").addEventListener("click", (event) => search
 document.getElementById("wiki_btn").addEventListener("click", (event) => search(dicts[5]));
 document.getElementById("youglish_btn").addEventListener("click", (event) => {
     let search_term = document.getElementById("search_term").value;
-    if (!search_term) return;
+    if (!search_term) {
+        alert("Input box empty!\nEnter your search...");
+        return;
+    }
     window.open(dicts[6] + search_term + "/english", "_blank");
+});
+
+// Populate the select element with options from the object
+const checkboxes = document.getElementById("checkboxes");
+for (const [name, url] of Object.entries(search_links)) {
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    input.value = url;
+
+    const label = document.createElement("label");
+    label.textContent = " " + name;
+    label.insertBefore(input, label.firstChild);
+    checkboxes.appendChild(label);
+}
+
+const generated_buttons_div = document.getElementById("generate_link");
+const multiple_search_btn = document.getElementById("multiple_search_btn");
+multiple_search_btn.addEventListener("click", () => {
+    const word = document.getElementById("search_term").value;
+    if (!word) {
+        alert("Input box empty!\nEnter your search, and try again");
+        return;
+    }
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    if (checkboxes.length === 0) {
+        alert("No options selected!\nSelect options, and try again");
+        return;
+    }
+
+    generated_buttons_div.innerHTML = "";
+    checkboxes.forEach((checkbox) => {
+        const url = checkbox.value.replace(/\{search_term\}/, word);
+        const btnText = checkbox.parentNode.textContent;
+
+        const link = document.createElement("a");
+        link.href = url;
+        link.target = "_blank";
+        link.textContent = btnText;
+        generated_buttons_div.appendChild(link);
+    });
+});
+
+const multiple_search_clear = document.getElementById("multiple_search_clear");
+multiple_search_clear.addEventListener("click", () => {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    checkboxes.forEach((checkbox) => (checkbox.checked = false));
+    generated_buttons_div.innerHTML = "";
 });
