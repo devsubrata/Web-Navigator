b7b0da265bbcf73e9659285e4c881d2e335dca0c
devsubrata <devsubrata@gmail.com>
2026-04-06 09:26:04 +0600

    improved image-viewer added for raading.html

---
+++ Link/resource/imgList.js
@@ -0,0 +1,16 @@
+const ReadingArticle = [
+    "Read ➜ Analysis ➜ Write.png",
+    "Read ➜ Analysis ➜ Write - 2.png",
+    "ExtractPattern.png",
+    "IELTS Reading Topics.png",
+    "Reading IELTS Articles.png",
+];
+const Motivation = [
+    "21 Laws of Self-Mastery.jpg",
+    "How to Believe in Yourself.jpg",
+    "How to wake up early without feeling tired.jpg",
+    "Motivation.jpg",
+    "Steps to build your personal growth plan.jpg",
+    "The 7 Pillars of Resilience.jpg",
+];
+const Mix = ["Career.png", "IELTS - Strategy.png"];
